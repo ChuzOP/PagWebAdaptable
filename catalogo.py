68b771aa24290f2cb6d3c74cb3fb9a899fcbd4ac
Chus <demiguelgarcia2003@gmail.com>
2022-04-18 23:42:32 -0600

    Entrar

--- catalogo.py
+++ catalogo.py
@@ -5,8 +5,15 @@ app = Flask(__name__) #creamos una variable
 @app.route("/")
 def home():
     return render_template('PWindex.html')
+
+@app.route("/aboutme")
+def about():
+    return render_template('about.html')
     
-  
+# @app.route("/tgraficas")
+# def graficas():
+#     return render_template('graficas.html')
+   
 if __name__ == "__main__":
     app.run(debug=True) #ponemos nuestra web en modo prueba
 
